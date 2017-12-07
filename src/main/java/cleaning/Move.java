4807aa3d18780d4303286677bcf93312b4b3f497
philba <p.bayer@freemail.hu>
2017-12-07 10:05:08 +0100

    added 'clean' move

--- src/main/java/cleaning/Move.java
+++ src/main/java/cleaning/Move.java
@@ -4,10 +4,13 @@ package cleaning;
  *
  */
 public enum Move {
+    Clean(0, 0),
     Left(-1, 0),
     Right(1, 0),
     Up(0, 1),
     Down(0, -1);
+
+    public static final Move[] movingMoves = {Left, Right, Up, Down};
 
     public final int x;
     public final int y;
