32a807e59724efe7b7457af48ad338ec8903f24a
philba <p.bayer@freemail.hu>
2017-12-08 14:38:00 +0100

    solve impl

--- src/main/scala/cleaning/Solver.scala
+++ src/main/scala/cleaning/Solver.scala
@@ -1,14 +1,52 @@
 package cleaning
 
+import scala.annotation.tailrec
+
 class Solver {
 
   def solve(botStart: String, board: Seq[String]) = {
-    val staringBoard = Board(botStart, staringBoard)
-
+    val staringBoard = Board(botStart, board)
+    solveRec(Stream((staringBoard, Seq.empty)), Seq.empty)
+      .filter(_._1.isClean)
+      .filter(_._2.head == Move.Clean)
+      .take(1)
+      .toList
   }
 
+  @tailrec
+  private def solveRec(boardsAndMoves: Stream[(Board, Seq[Move])],
+                       boardHistory: Seq[Board]): Stream[(Board, Seq[Move])] = {
+    //    val newBoardsAndMoves = streamOfMoves(boardsAndMoves, boardHistory)
+    //    val newBoardHistory = boardHistory ++ newBoardsAndMoves.map(_._1)
 
-  def doAllPossibleMoves(currentBoard: Board, movesHistory: Seq[Move], boardHistory: Seq[Board]): Seq[(Board, Move)] = {
+    val stream: Seq[(Board, Seq[Move])] = for {
+      currentBoardsAndMoves <- boardsAndMoves
+      newBoardsAndMoves <- streamOfMoves(currentBoardsAndMoves, boardHistory)
+    } yield {
+      newBoardsAndMoves
+    }
+    val newBoardHistory = boardHistory ++ stream.map(_._1)
+
+    //    boardsAndMoves.append(solveRec(stream, newBoardHistory))
+    //    solveRec(boardsAndMoves.append(stream), newBoardHistory)
+    solveRec(boardsAndMoves ++ stream, newBoardHistory)
+  }
+
+  private def streamOfMoves(currentBoardAndMoves: (Board, Seq[Move]), boardHistory: Seq[Board]) = {
+    val (currentBoard, moves) = currentBoardAndMoves
+    if (currentBoard.isClean) {
+      Seq.empty
+    }
+
+    val newMoves = doAllPossibleMoves(currentBoard)
+
+    val newBoardsAndMoves = newMoves.withFilter({ case (board, _) => boardHistory.contains(board) == false })
+      .map({ case (board, move) => (board, move +: moves) })
+
+    newBoardsAndMoves
+  }
+
+  private def doAllPossibleMoves(currentBoard: Board): Seq[(Board, Move)] = {
     val validMoves = currentBoard.validMoves
     validMoves.map(move => (currentBoard.moveBot(move), move._2))
   }
