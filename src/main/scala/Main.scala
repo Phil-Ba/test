efcc883ab8d555de3acbe97bda77951c26b5ea24
philba <p.bayer@freemail.hu>
2017-12-10 23:06:16 +0100

    tailrec experiments

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,37 @@
+import java.nio.file.{Files, Paths}
+import java.time.{Instant, Duration => jDur}
+import java.util.concurrent.TimeUnit
+
+import cleaning.Solver
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+/**
+  *
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    val start = Instant.now()
+
+    import scala.collection.JavaConverters._
+    import scala.concurrent.ExecutionContext.Implicits._
+
+    val task = Future {
+      val solution = new Solver().solve("0,0", Array(
+        "----d",
+        "----d",
+        "----d",
+        "----d",
+        "----d")
+      )
+      val end = Instant.now()
+      jDur.between(start, end)
+      solution.map(x => (s"moves(${x._2.size}) = ${x._2.reverse}"))
+    }.map { x =>
+      Files.write(Paths.get("lines.txt"), x.asJava)
+    }
+
+    Await.result(task, Duration(2, TimeUnit.MINUTES))
+  }
+}
--- src/main/scala/cleaning/Solver.scala
+++ src/main/scala/cleaning/Solver.scala
@@ -1,26 +1,37 @@
 package cleaning
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 class Solver {
 
+  type boardWithMoves = (Board, Seq[Move])
+
   def solve(botStart: String, board: Seq[String]) = {
     val staringBoard = Board(botStart, board)
-    solveRec(Stream((staringBoard, Seq.empty)), Seq.empty)
-      .filter(_._1.isClean)
-      .filter(_._2.head == Move.Clean)
-      .take(1)
+    solveRec(Stream.empty, mutable.Queue((staringBoard, Seq.empty)), Seq.empty)
+      .filter(x => x._1.isClean && x._2.head == Move.Clean)
+      //            .filter(_._1.isClean)
+      //      .filter(_._2.head == Move.Clean)
+      .take(5)
       .toList
   }
 
   @tailrec
-  private def solveRec(boardsAndMoves: Stream[(Board, Seq[Move])],
+  private def solveRec(boardsAndMoves: Stream[boardWithMoves], workStack: mutable.Queue[boardWithMoves],
                        boardHistory: Seq[Board]): Stream[(Board, Seq[Move])] = {
     //    val newBoardsAndMoves = streamOfMoves(boardsAndMoves, boardHistory)
     //    val newBoardHistory = boardHistory ++ newBoardsAndMoves.map(_._1)
+    if (workStack.isEmpty) {
+      println("finished?!")
+      return boardsAndMoves
+    }
+    println(workStack.size)
+
+    val currentBoardsAndMoves = workStack.dequeue()
 
     val stream: Seq[(Board, Seq[Move])] = for {
-      currentBoardsAndMoves <- boardsAndMoves
+      //      currentBoardsAndMoves <- workStack.dequeue()
       newBoardsAndMoves <- streamOfMoves(currentBoardsAndMoves, boardHistory)
     } yield {
       newBoardsAndMoves
@@ -29,10 +40,11 @@ class Solver {
 
     //    boardsAndMoves.append(solveRec(stream, newBoardHistory))
     //    solveRec(boardsAndMoves.append(stream), newBoardHistory)
-    solveRec(boardsAndMoves ++ stream, newBoardHistory)
+    workStack.enqueue(stream: _*)
+    solveRec(boardsAndMoves ++ (stream), workStack, newBoardHistory)
   }
 
-  private def streamOfMoves(currentBoardAndMoves: (Board, Seq[Move]), boardHistory: Seq[Board]) = {
+  private def streamOfMoves(currentBoardAndMoves: boardWithMoves, boardHistory: Seq[Board]) = {
     val (currentBoard, moves) = currentBoardAndMoves
     if (currentBoard.isClean) {
       Seq.empty
@@ -40,7 +52,8 @@ class Solver {
 
     val newMoves = doAllPossibleMoves(currentBoard)
 
-    val newBoardsAndMoves = newMoves.withFilter({ case (board, _) => boardHistory.contains(board) == false })
+    val newBoardsAndMoves = newMoves
+      .withFilter({ case (board, _) => boardHistory.contains(board) == false })
       .map({ case (board, move) => (board, move +: moves) })
 
     newBoardsAndMoves
